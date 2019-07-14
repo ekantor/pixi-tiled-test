411f78dc247084269118ec781f02eb677e6ca2d3
Kántor Endre <kaendre@gmail.com>
2019-07-14 15:51:05 +0200

    player

--- index.ts
+++ index.ts
@@ -1,13 +1,32 @@
 import * as PIXI from 'pixi.js';
 import TILEDGraphics from './TILEDGraphics';
 
+const p2 = require('p2');
+
+const shape = new p2.Circle({ radius: 32 });
+
+const body = new p2.Body({ mass: 1 });
+body.addShape(shape);
+
+const world = new p2.World({
+	gravity: [0, 0],
+});
+world.addBody(body);
+
+
 window.onload = () => {	
-	const path = 'assets/1v1/1v1_field.json';
+	const path = 'assets/world/map.json';
 
 	const app = new PIXI.Application({ width: 800, height: 600 });
 	
+	const player = new PIXI.Graphics();
+	player.beginFill(0x0000ff);
+	player.drawCircle(0, 0, 32);
+	player.endFill();
+
 	fetch(path).then(res => res.json()).then(tilemap => {
 		app.stage.addChild(new TILEDGraphics(tilemap, path));
+		app.stage.addChild(player);
 	});
 	
 	document.body.appendChild(app.view);
@@ -17,19 +36,22 @@ window.onload = () => {
 	document.addEventListener('keyup', e => keys[e.key] = false);
 
 	app.ticker.add(() => {
-		const v = 10;
+		const v = 5;
+
+		body.velocity[0] = body.velocity[1] = 0;
 		if (keys['ArrowLeft'] || keys['a']) {
-			app.stage.x += v;
+			body.velocity[0] -= v;
 		}
 		if (keys['ArrowRight'] || keys['d']) {
-			app.stage.x -= v;
+			body.velocity[0] += v;
 		}
 		if (keys['ArrowDown'] || keys['s']) {
-			app.stage.y -= v;
+			body.velocity[1] += v;
 		}
 		if (keys['ArrowUp'] || keys['w']) {
-			app.stage.y += v;
+			body.velocity[1] -= v;
 		}
+
 		const zoom = 1.1;
 		if (keys['z'] || keys['y']) {
 			app.stage.scale.x *= zoom;
@@ -39,6 +61,14 @@ window.onload = () => {
 			app.stage.scale.x /= zoom;
 			app.stage.scale.y /= zoom;
 		}
+
+		world.step(app.ticker.deltaMS);
+
+		player.x = body.position[0];
+		player.y = body.position[1];
+
+		app.stage.x = 400 - player.x;
+		app.stage.y = 300 - player.y;
 	});
 
 	app.start();
